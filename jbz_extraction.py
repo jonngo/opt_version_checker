8777f8a48cec3a33ff52b28682325e8786983ec6
Jon Ngo <jonathan.ngo@invenco.com>
2020-05-23 20:26:34 +1200

    Extraction refactored, and with full path option. Not limited only for jbz, can also extract a tgz with multiple layer of packages inside.

--- jbz_extraction.py
+++ jbz_extraction.py
@@ -7,22 +7,23 @@ from subprocess import Popen,PIPE
 
 class JobBundleExtraction:
     def __init__(self):
-        self.base_xtrak_path = "D:/JonNgoStorage/version_checker/"
+        self.base_xtrak_path = "D:/JonNgoStorage/my_hack_project/opt_version_checker/"
         self.sevenZ_exe = "C:/Program Files/7-Zip/7z.exe"
-        self.xtrak_path1 = self.base_xtrak_path + "xtrak_level_1/"
-        self.xtrak_path2 = self.base_xtrak_path + "xtrak_level_2/"
-        self.xtrak_path3 = self.base_xtrak_path + "xtrak_level_3/"
+        self.extract_folder = "xtrak_level_"
+        self.extract_file_starting_index = 1
+        self.extract_file_ending_index = 10
         self.manifest_file = "manifest.mnf"
         self.regexp_string = r"\d{2}.\d{2}.\d{4}"
-        try:
-            shutil.rmtree(self.xtrak_path1)
-            shutil.rmtree(self.xtrak_path2)
-            shutil.rmtree(self.xtrak_path3)
-        except:
-            print('Error while deleting directory')
+        self.use_full_path = False
+
+        for x in range(self.extract_file_starting_index,self.extract_file_ending_index+1):
+            if os.path.exists(self.base_xtrak_path+self.extract_folder+str(x)):
+                try:
+                    shutil.rmtree(self.base_xtrak_path+self.extract_folder+str(x))
+                except:
+                    print('Error while deleting directory')
 
     def filenameOnly(self,f):
-        print ('filenameonly')
         return os.path.splitext(f.split('/')[-1])[0]
 
     def set_regex_to_find_version(self,regex_in):
@@ -45,73 +46,48 @@ class JobBundleExtraction:
             return pkg_name1
 
     def extract(self, jbz_file):
-        # Level 1 extraction
-        process = Popen([self.sevenZ_exe, "x", jbz_file, "-o" + self.xtrak_path1, "*"], stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
+        pkg_list = [jbz_file, ]
 
-        s = stdout.decode("utf-8")
-        print(s)
-        #filter all pkg
-        # pkg_list = [os.path.join(root,name) for root, dirs, files in os.walk(self.xtrak_path1) for name in files if os.path.join(root, name).endswith(".pkg")]
-        pkg_list = [os.path.join(root,name) for root, dirs, files in os.walk(self.xtrak_path1) for name in files]
+        for x in range(self.extract_file_starting_index,self.extract_file_ending_index+1):
+            for p in pkg_list:
+                print (p)
+                process = Popen([self.sevenZ_exe,"x",p,"-o"+self.base_xtrak_path+self.extract_folder+str(x)+'/'+self.filenameOnly(p)+"/","*"], stdout=PIPE, stderr=PIPE)
+                stdout, stderr = process.communicate()
+                s = stdout.decode("utf-8")
+                print(s)
 
-        #Level 2 extraction
-        for p in pkg_list:
-            print (p)
+            #If the extract folder is not created then break from the loop
+            if not os.path.exists(self.base_xtrak_path+self.extract_folder+str(x)):
+                break;
 
-            process = Popen([self.sevenZ_exe,"x",p,"-o"+self.xtrak_path2+self.filenameOnly(p)+"/","*"], stdout=PIPE, stderr=PIPE)
-            #process = Popen([self.sevenZ_exe, "x", p, "-o" + self.xtrak_path2 + (os.path.splitext(p.split('/')[-1])[0]) + "/", "*"], stdout=PIPE, stderr=PIPE)
-            stdout, stderr = process.communicate()
-
-            s = stdout.decode("utf-8")
-            print(s)
-            print("-"*150)
-
-        #filter all tar and cpio
-        # pkg_list = [os.path.join(root,name) for root, dirs, files in os.walk(self.xtrak_path2) for name in files if os.path.join(root,name).endswith(".tar") or os.path.join(root,name).endswith(".cpio")]
-        pkg_list = [os.path.join(root,name) for root, dirs, files in os.walk(self.xtrak_path2) for name in files]
-
-        #Level 3 extraction
-        for p in pkg_list:
-            print (p)
-
-            process = Popen([self.sevenZ_exe,"x",p,"-o"+self.xtrak_path3+self.filenameOnly(p)+"/","*"], stdout=PIPE, stderr=PIPE)
-            stdout, stderr = process.communicate()
-
-            s = stdout.decode("utf-8")
-            print(s)
-            print("-"*150)
+            pkg_list = [os.path.join(root, name) for root, dirs, files in os.walk(self.base_xtrak_path+self.extract_folder+str(x)+'/') for name in files]
 
         master_pkg_list = []
         master_pkg_list.append(['Package','Version'])
 
-        all_xtrak = [self.xtrak_path3,self.xtrak_path2,self.xtrak_path1]
+        for x in range(self.extract_file_starting_index,self.extract_file_ending_index+1):
+            if os.path.exists(self.base_xtrak_path + self.extract_folder + str(x)):
+                print('processing ... '+str(x))
+                for root, dirs, files in os.walk(self.base_xtrak_path+self.extract_folder+str(x)):
+                    for name in files:
+                        v = self.getVersion(name)
+                        print(v)
+                        if v is not None:
+                            if self.use_full_path:
+                                master_pkg_list.append(["".join(os.path.join(root, name).split('/')[4:]),v])
+                            else:
+                                master_pkg_list.append([name, v])
 
-        for x in all_xtrak:
-            print(x)
-            for root, dirs, files in os.walk(x):
-                for name in files:
-                    v = self.getVersion(name)
-                    print(v)
-                    if v is not None:
-                        # master_pkg_list.append(["".join(os.path.join(root, name).split('/')[4:]),v])
-                        master_pkg_list.append([name, v])
+        self.display_table(master_pkg_list)
 
         #Find manifest file
 
-        table = Texttable()
-        table.set_deco(Texttable.HEADER)
-        table.set_cols_dtype(['t', 't'])  # text
-        table.set_cols_width([180, 20])
+        manifest = None
 
-        table.set_cols_align(["l", "l"])
-        table.add_rows(master_pkg_list)
-        print(table.draw())
-
-        for x in all_xtrak:
-            for root, dirs, files in os.walk(x):
+        for x in range(self.extract_file_starting_index, self.extract_file_ending_index + 1):
+            for root, dirs, files in os.walk(self.base_xtrak_path + self.extract_folder + str(x)):
                 for name in files:
-                    if self.manifest_file in name:
+                    if self.manifest_file == name:
                         manifest_file = "".join(os.path.join(root, name))
 
                         try:
@@ -140,5 +116,26 @@ class JobBundleExtraction:
                         manifest = [[n[c].split('"')[1], v[c].split('"')[1]] for c in range(0, len(v))]
                         manifest.insert(0,['TAG','VERSION'])
 
+        if manifest is not None:
+            self.display_table(manifest)
         return master_pkg_list, manifest
+
+    def display_table(self, content):
+        table = Texttable()
+        table.set_deco(Texttable.HEADER)
+        table.set_cols_dtype(['t', 't'])  # text
+        table.set_cols_width([180, 20])
+
+        table.set_cols_align(["l", "l"])
+        table.add_rows(content)
+        print(table.draw())
+
+# Uncomment to perform module test.
+# if __name__ == "__main__":
+#     file_to_extract = 'D:/JonNgoStorage/sequioa/R3.2.1/02.02.0050/R3.2.1_seq_platform_dev_su_with_bm_qatest_02.02.0050.jbz'
+#     #file_to_extract = 'D:/JonNgoStorage/sequioa/R3.2.1/02.02.0050/R3.2.1_seq_factory_dev_02.02.0050.tgz'
+#     jbx = JobBundleExtraction()
+#     result_jbz, result_manifest = jbx.extract(file_to_extract)
+#     if result_manifest is None:
+#         print('No manifest file found')
 
